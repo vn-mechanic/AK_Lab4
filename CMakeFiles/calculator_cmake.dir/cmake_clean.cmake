4df2b1df666195fda4ff7e19842bd5d2cfac58b0
vn-mechanic <vn.mechanic@gmail.com>
2019-12-24 11:46:33 +0300

    final
    ak lab4 cmake

---
+++ CMakeFiles/calculator_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator_cmake.dir/calculator.cpp.o"
+  "calculator_cmake.pdb"
+  "calculator_cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/calculator_cmake.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for calculator_cmake.
+# This may be replaced when dependencies are built.
